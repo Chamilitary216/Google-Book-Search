835462468782bfd51eee05d02408b5c30c355f88
Chris Curry <Chamilitary216@hotmail.com>
2021-05-16 19:40:12 -0400

    created NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import SavedPage from "./pages/SavedPage/SavedPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Navbar from "./components/Navbar/Navbar";
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/saved" component={SavedPage} />
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Style.css"
+import { Link } from "react-router-dom";
 
-function Navbar() {
+
+const Navbar = () => {
+
+    const [nav, setnav] = useState (false);
+
+    const changeBackground = () => {
+        if (window.scrollY >= 50) {
+            setnav (true);
+
+        }else {
+            setnav (false);
+        }
+        
+    }
+
+    window.addEventListener ("scroll", changeBackground);
+
     return (
-        <div>
-            
-        </div>
+        <nav className = {nav ? "nav active" : "nav"}>
+        
+        <Link to = "#" className = "logo">
+            <img src = {logo} alt = ''/>
+        </Link>
+        
+        <input className = "menu-btn" type = "checkbox" id = "menu-btn"/>
+        <label className = "menu-icon" htmlFor = "menu-btn">
+            <span className = "nav-icon"></span>
+        </label>
+
+    <ul className = "menu" >
+        <li><Link to = "/home"> Home </Link></li>
+        <li><Link to = "/saved"> Saved Books </Link></li>
+    </ul>
+
+    </nav>
+
     )
 }
 
-export default Navbar
+export default Navbar;
